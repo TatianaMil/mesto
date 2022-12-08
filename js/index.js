53d2eed89b1927a4f7076fe084ea9b4808e4f81b
TatianaMil <ftv9988.it@gmail.com>
2022-12-08 21:24:13 +0400

    feat: добавлен js файл

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// .profile__button = profileButton
+// .popup
+// .popup__button-close
+
+const profileButton = document.querySelector('.profile__button');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup__button-close');
+
+profileButton.addEventListener('click', function() {
+  popup.classList.add('popup_opened');
+});
+
+popupClose.addEventListener('click', function() {
+  popup.classList.remove('popup_opened');
+});
+
+//находим форму в дом
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_form_name');
+let aboutInput = document.querySelector('.popup__input_form_about');
+let profileTitle = document.querySelector('.profile__title');
+let profileInfo = document.querySelector('.profile__info');
+let submitButton = document.querySelector('.popup__button-submit');
+
+//обработчик отправки формы
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileInfo.textContent = aboutInput.value;
+  popup.classList.remove('popup_opened');
+};
+
+formElement.addEventListener('submit', handleFormSubmit);
