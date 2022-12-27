3d6536cc942c77e0f97dceb184d9dd5fcdd054f5
TatianaMil <ftv9988.it@gmail.com>
2022-12-27 16:48:51 +0400

    fixed: afte 1 iteration

--- js/index.js
+++ js/index.js
@@ -2,25 +2,23 @@ const profileButton = document.querySelector('.profile__button');
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__button-close');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input_form_name');
-let aboutInput = document.querySelector('.popup__input_form_about');
+let nameInput = document.querySelector('.popup__input_form-name');
+let aboutInput = document.querySelector('.popup__input_form-about');
 let profileTitle = document.querySelector('.profile__title');
 let profileInfo = document.querySelector('.profile__info');
-let submitButton = document.querySelector('.popup__button-submit');
 
 //5sprint объявили переменные для добавления в кнопку плюс
-const buttonPlus = document.querySelector('.profile__button-plus');
-const popupImgName = document.querySelector('.popup__input_form_img-name');
-const popupImgLink = document.querySelector('.popup__input_form_img-link');
-const popupImg = document.querySelector('.popup__img');
-const popupTitleImg = document.querySelector('.popup__title-img');
-const popupCardImg = document.querySelector('.popup_type_img');
-const popupPlacePluse = document.querySelector('.popup_place_pluse');
-const popupBtnClosePlacePluse = document.querySelector('.popup__button-close_place_pluse');
-const popupBtnSubmitPlacePluse  = document.querySelector('.popup__button-submit_place_pluse');
-const galleryTitle = document.querySelector('.gallery__title');
-const popupFormPlacePluse = document.querySelector('.popup__form_place_pluse');
-const popupButtonClosePlaceImg = document.querySelector('.popup__button-close_place_big-img')
+const buttonOpeneFormCard = document.querySelector('.profile__button-plus');
+const popupImageName = document.querySelector('.popup__input_img-name');
+const popupImageLink = document.querySelector('.popup__input_img-link');
+const popupImage = document.querySelector('.popup__img');
+const popupTitleImage = document.querySelector('.popup__title-img');
+const popupCardImage = document.querySelector('.popup_type_img');
+const popupAddCard = document.querySelector('.popup_type_card-add');
+const popupButtonCloseAddCard = document.querySelector('.popup__button-close_add-card');
+const popupFormPlacePluse = document.querySelector('.popup__form_card-add');
+const popupButtonClosePlaceImg = document.querySelector('.popup__button-close_big-img');
+
 
 //открытие попапа для имени и about
 profileButton.addEventListener('click', function() {
@@ -28,13 +26,14 @@ profileButton.addEventListener('click', function() {
   nameInput.value = profileTitle.textContent;
   aboutInput.value = profileInfo.textContent;
 });
+
 //закрытие попапа при клике на крестик (для всех попапов)
 popupClose.addEventListener('click', function() {
   popup.classList.remove('popup_opened');
 });
 
 //обработчик отправки формы
-function handleFormSubmit(evt) {
+const handleFormSubmit = (evt) => {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileInfo.textContent = aboutInput.value;
@@ -71,16 +70,17 @@ const initialCards = [
 ];
 
 //открытие и закрытие попапа по нажатию на плюс
-buttonPlus.addEventListener('click', () => {
-  popupPlacePluse.classList.add('popup_opened');
+buttonOpeneFormCard.addEventListener('click', () => {
+  popupAddCard.classList.add('popup_opened');
 });
-popupBtnClosePlacePluse.addEventListener('click', () => {
-  popupPlacePluse.classList.remove('popup_opened');
+
+popupButtonCloseAddCard.addEventListener('click', () => {
+  popupAddCard.classList.remove('popup_opened');
 });
 
 //закрытие попапа большой картинки
 popupButtonClosePlaceImg.addEventListener('click', () => {
-  popupCardImg.classList.remove('popup_opened');
+  popupCardImage.classList.remove('popup_opened');
 });
 
 //добавление и удаление лайка
@@ -90,20 +90,19 @@ const handleLikeClick = (evt) => {
 
 //функция открытия большой картинки
 const openBigImg = (img, title) => {
-    popupCardImg.classList.add('popup_opened');
-    popupImg.src = img;
-    popupImg.alt = title;
-    popupTitleImg.textContent = title;
+  popupCardImage.classList.add('popup_opened');
+  popupImage.src = img;
+  popupImage.alt = title;
+  popupTitleImage.textContent = title;
 };
 
 // создание новой карточки
 const galleryContainer = document.querySelector('.gallery');
 const template = document.querySelector('#template-new-img');
-//для открытия картинки
 
+//для открытия картинки
 const createNewCard = (name, link) => {
   const newCard = template.content.querySelector('.gallery__item').cloneNode(true);
-  const galleryImg = document.querySelector('.gallery__img');
 
   newCard.querySelector('.gallery__title').textContent = name;
   newCard.querySelector('.gallery__img').src = link;
@@ -113,9 +112,9 @@ const createNewCard = (name, link) => {
   newCard.querySelector('.gallery__heart').addEventListener('click', handleLikeClick);
 
   //для удаления карточки по нажатию на корзину
-  const deleteCard = newCard.querySelector(".gallery__del")
-  deleteCard.addEventListener("click", () => {
-  newCard.remove();
+  const deleteCardButton = newCard.querySelector('.gallery__del');
+  deleteCardButton.addEventListener('click', () => {
+    newCard.remove();
   });
 
   //большая картинка
@@ -139,10 +138,13 @@ initialCards.forEach ((element) => {
 //добавление своей карточки в галерею
 const addCardSubmit = (evt) => {
   evt.preventDefault();
-  renderCard(popupImgName.value, popupImgLink.value);
-  popupPlacePluse.classList.remove('.popup__form_place_pluse');
-  popupPlacePluse.classList.remove('popup_opened');
+  renderCard(popupImageName.value, popupImageLink.value);
+  popupAddCard.classList.remove('popup__form_card-add');
+  popupAddCard.classList.remove('popup_opened');
   popupFormPlacePluse.reset();
 }
 
 popupFormPlacePluse.addEventListener('submit', addCardSubmit);
+
+
+
