76b20a83c282ed62995912138e187fa038da079b
TatianaMil <ftv9988.it@gmail.com>
2023-02-14 19:40:06 +0400

    added webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import {Card} from './Card.js';
-import {FormValidator} from './FormValidator.js'
+import {Card} from '../components/Card.js';
+import {FormValidator} from '../components/FormValidator.js'
+import './index.css'
 
 const profileButton = document.querySelector('.profile__button');
 const popupProfile = document.querySelector('.popup_type_profile');
